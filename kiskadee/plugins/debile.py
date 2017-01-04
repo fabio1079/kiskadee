ab55cb20e1a1492f42c6aadd7bb9d52a6bd73a65
David Carlos <ddavidcarlos1392@gmail.com>
2017-01-04 17:29:16 -0200

    Enable to run a command in slave container.

--- kiskadee/plugins/debile.py
+++ kiskadee/plugins/debile.py
@@ -14,9 +14,10 @@ class Runner():
 
     def __init__(self):
         self.client = docker.from_env()
-        self.docker_root = os.path.dirname(os.path.realpath(__file__)) + "/../%s" % runner_dir
-        self.synced_root = os.path.dirname(os.path.realpath(__file__)) + "/../%s" % runner_name
-        self.container = None
+        self.slave = None
+        self.master = None
+        self.pg = None
+        self.data = None
 
     def watch(self):
         pass
@@ -24,39 +25,56 @@ class Runner():
     def callback(self):
         pass
 
-    def valid_env(self):
-        env_docker = call('docker', shell=True)
-        return env_docker <= 0
-
     def container_id(self):
         call(['docker', 'ps -aqf "name=debile-pg"'], shell=True)
 
-    def check_master(self):
+    def check_debile(self, container):
         try:
-            self.container = self.client.containers.get('debile-master')
-            self.container.start()
-            return self.container
+            debile_container = self.client.containers.get('debile-%s' % container)
+            debile_container.start()
+            setattr(self, container, debile_container)
+            return True
         except Exception:
             print('Container not created')
             return False
-        pass
 
     def start_master(self):
-        if not (self.check_master()):
-            print('Creating container')
-            self.container = self.client.containers.run("clemux/debile-master", "tail -f /dev/null",
-                name='debile-master',detach=True,
-                volumes={self.synced_root: {'bind': '/srv/debile/sync_repo', 'mode': 'rw'}})
+        if not (self.check_debile('master')):
+            print('Starting debile master')
+            self.master = self.client.containers.run("debile-master-pkg",
+                                                    "debile-master --config /etc/debile/master.yaml" \
+                                                    "--sign simple",
+                    name='debile-master', links={'debile-pg':'debile-pg'},
+                    volumes_from=['debile-data'])
 
-        return self.container
+    def start_slave(self):
+        if not (self.check_debile('slave')):
+            print('Starting debile slave')
+            self.slave = self.client.containers.run("debile-slave-pkg", "tail -f /dev/null",
+                                   name='debile-slave', detach=True)
 
-    def run(self):
-        print(self.container.exec_run("ls"))
+    def start_pg(self):
+        if not(self.check_debile('pg')):
+            self.pg = self.client.containers.run("clemux/debile-pg", "tail -f /dev/null",
+                                   name='debile-pg', detach=True)
+    def start_data(self):
+        if not(self.check_debile('data')):
+            self.pg = self.client.containers.run("clemux/debile-data", "tail -f /dev/null",
+                                   name='debile-data', detach=True)
+
+    def start_debile(self):
+        self.start_pg()
+        self.start_data()
+        self.start_master()
+        self.start_slave()
+
+    def call(self):
+        print(self.slave.exec_run("debile-upload --dist=unstable --source=vim --version=7.3.547-7 --group=test"))
 
         #TODO: docker python lib cannot stop container properly
         call(['docker', 'stop', 'debile-master'])
 
 if __name__ == "__main__":
     runner = Runner()
-    container = runner.start_master()
-    runner.run()
+    runner.start_debile()
+    runner.call()
