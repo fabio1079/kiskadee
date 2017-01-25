1e96b7873f8499f1904922f13889e5260ed0d771
David Carlos <ddavidcarlos1392@gmail.com>
2017-01-25 09:58:12 -0200

    Upload package to debile-master with debile-slave

--- kiskadee/plugins/debile.py
+++ kiskadee/plugins/debile.py
@@ -18,6 +18,7 @@ class Runner():
         self.master = None
         self.pg = None
         self.data = None
+        self.http = None
 
     def watch(self):
         pass
@@ -28,53 +29,81 @@ class Runner():
     def container_id(self):
         call(['docker', 'ps -aqf "name=debile-pg"'], shell=True)
 
-    def check_debile(self, container):
+    def retrieve_container(self, container_name):
         try:
-            debile_container = self.client.containers.get('debile-%s' % container)
-            debile_container.start()
-            setattr(self, container, debile_container)
-            return True
+            container = self.containers().get('debile-%s' % container_name)
+            setattr(self, container_name, container)
+            self.start_container(container)
         except Exception:
-            print('Container not created')
-            return False
+            print("Something went wrong and " +
+                  "container was not properly started")
+            method_call = getattr(self, "run_%s" % container_name)
+            method_call()
 
-    def start_master(self):
-        if not (self.check_debile('master')):
+    def start_container(self, container):
+        try:
+            container.start()
+            return container
+        except Exception:
+            print('Container already runnin')
+            return container
+
+    def check(self):
+        containers = ['data', 'pg', 'http',
+                      'master', 'slave']
+        for container in containers:
+            self.retrieve_container(container)
+            self.start_container(container)
+
+    def run_master(self):
             print('Starting debile master')
-            self.master = self.client.containers.run("debile-master-pkg",
-                                                    "debile-master --config /etc/debile/master.yaml" \
-                                                    "--sign simple",
-                    name='debile-master', links={'debile-pg':'debile-pg'},
-                    volumes_from=['debile-data'])
+            self.master = self.containers().run("debile-master-pkg",
+                                                "debile-master --config " +
+                                                "/etc/debile/master.yaml " +
+                                                "--auth simple",
+                                                name='debile-master',
+                                                links={'debile-pg': 'debile-pg'},
+                                                volumes_from=['debile-data'],
+                                                detach=True)
 
-    def start_slave(self):
-        if not (self.check_debile('slave')):
+    def run_slave(self):
             print('Starting debile slave')
-            self.slave = self.client.containers.run("debile-slave-pkg", "tail -f /dev/null",
-                                   name='debile-slave', detach=True)
+            self.slave = self.containers().run("debile-slave-pkg",
+                                               "tail -f /dev/null",
+                                               links={'debile-http': 'debile-http',
+                                                      'debile-master': 'debile-master'},
+                                               name='debile-slave',
+                                               detach=True)
 
-    def start_pg(self):
-        if not(self.check_debile('pg')):
-            self.pg = self.client.containers.run("clemux/debile-pg", "tail -f /dev/null",
-                                   name='debile-pg', detach=True)
-    def start_data(self):
-        if not(self.check_debile('data')):
-            self.pg = self.client.containers.run("clemux/debile-data", "tail -f /dev/null",
-                                   name='debile-data', detach=True)
+    def run_pg(self):
+            self.pg = self.containers().run("clemux/debile-pg",
+                                            "tail -f /dev/null",
+                                            name='debile-pg',
+                                            ports={'5432': '5432'},
+                                            detach=True)
+    def run_data(self):
+            self.pg = self.containers().run("clemux/debile-data",
+                                            "tail -f /dev/null",
+                                            name='debile-data',
+                                            detach=True)
 
-    def start_debile(self):
-        self.start_pg()
-        self.start_data()
-        self.start_master()
-        self.start_slave()
+    def run_http(self):
+            self.http = self.containers().run("clemux/debile-http",
+                                              "tail -f /dev/null",
+                                              name='debile-http',
+                                              volumes_from=['debile-data'],
+                                              ports={'80': '80'},
+                                              detach=True)
 
-    def call(self):
-        print(self.slave.exec_run("debile-upload --dist=unstable --source=vim --version=7.3.547-7 --group=test"))
+    def upload(self):
+        print(self.slave.exec_run("debile-upload --dist=unstable " +
+                                  "--source=mutt --version=1.7.1-5 " +
+                                  "--group=default"))
 
-        #TODO: docker python lib cannot stop container properly
-        call(['docker', 'stop', 'debile-master'])
+    def containers(self):
+        return self.client.containers
 
 if __name__ == "__main__":
-    runner = Runner()
-    runner.start_debile()
-    runner.call()
+    debile = Runner()
+    debile.check()
+    debile.upload()
