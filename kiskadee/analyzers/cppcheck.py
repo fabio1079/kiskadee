b498135979791b6df74a099c92ab4d211c73bed4
David Carlos <ddavidcarlos1392@gmail.com>
2017-04-24 23:35:38 -0300

    Initial use of firehose parsers.

--- kiskadee/analyzers/cppcheck.py
+++ kiskadee/analyzers/cppcheck.py
@@ -6,12 +6,13 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-from firehose.model import Issue, Message, File, Location, Point
+from firehose.parsers.cppcheck import parse_file
 import tempfile
 import os
 from subprocess import check_output
 import subprocess
 import pdb
+from io import BytesIO
 
 
 def cppcheck(source_dir):
@@ -19,37 +20,47 @@ def cppcheck(source_dir):
     :source_dir: Absolute path to source code
     :returns: Write cppcheck report inside kiskadee/reports directory
     """
-    report_directory = os.path.join(os.path.abspath("."), "reports/")
-    try:
-        os.mkdir(report_directory)
-    except OSError:
-        pass
 
-    report_file = "cppcheck_report.xml"
     initial_dir =os.getcwd()
     # TODO: Use 'with' to change to the extracted
     # source directory, instead of os.getcwd()
     os.chdir(source_dir)
-    pipes = subprocess.Popen([ 'cppcheck', '-j8', '--enable=all', 
-                               '--xml-version=2', '.'], 
-                               stdout=subprocess.PIPE, 
+    pipes = subprocess.Popen([ 'cppcheck', '-j8', '--enable=all',
+                               '--xml-version=2', '.'],
+                               stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
 
-    std_out, std_err = pipes.communicate()
-
-    f = open(report_directory + report_file, 'w')
-    f.write(std_err.decode('utf-8'))
-    f.close
     os.chdir(initial_dir)
-    
+    _, std_err = pipes.communicate()
+    return to_firehose(std_err)
 
 
 def to_firehose(report):
     """ Parser the analyzer report to Firehose format
     :report: The analyzer report
-    :returns: Firehose report
+    :returns: create a firehose report inside reports directory
     """
-    pass
+
+    simple_report_file = "simple_cppcheck_report.xml"
+    firehose_report_file = "firehose_cppcheck_report.xml"
+    report_directory = os.path.join(os.path.abspath("."), "reports/")
+
+    try:
+        os.mkdir(report_directory)
+    except OSError:
+        pass
+
+    try:
+        f = open(report_directory + simple_report_file, 'w')
+        f.write(report.decode('UTF-8'))
+        f.close()
+    except IOError:
+        print("File %s not found", simple_report_file)
+
+    tree_analysis = parse_file(report_directory + simple_report_file).to_xml()
+    tree_analysis.write((report_directory  + firehose_report_file), encoding='UTF-8')
+    
+
 
 def version():
     try:
