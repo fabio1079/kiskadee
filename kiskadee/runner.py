a1facae36c86e2114b69a6fc8b5057d196cbc95c
David Carlos <ddavidcarlos1392@gmail.com>
2017-07-02 21:53:28 -0300

    Refactors analyze method.
        - The analyze method was to big, and was difficult to read. This commit
        breaks the method in other two, in order to facilitate it's
        behavior.
        - Creates _path_to_uncompressed_source method, responsible to
        download the source, uncompress it and return it's  absolute path.

--- kiskadee/runner.py
+++ kiskadee/runner.py
@@ -20,72 +20,63 @@ def runner():
     :func:`analyze` method, passing the dequeued package. After the analysis,
     updates the status of this package on the database.
     """
+    kiskadee.logger.debug('Starting runner component')
     while running:
         if not kiskadee.queue.is_empty():
             kiskadee.logger.debug('RUNNER: dequeuing...')
-            package = kiskadee.queue.dequeue_analysis()
+            source_to_analysis = kiskadee.queue.dequeue_analysis()
             kiskadee.logger.debug('RUNNER: dequeued %s-%s from %s'
-                                  % (package['name'],
-                                     package['version'],
-                                     package['plugin'].__name__))
-            analyze(package)
+                                  % (source_to_analysis['name'],
+                                     source_to_analysis['version'],
+                                     source_to_analysis['plugin'].__name__))
+            source_name = source_to_analysis['name']
+            pkg = (
+                    session.query( kiskadee.model.Package)
+                    .filter(kiskadee.model.Package.name == source_name)
+                    .first()
+            )
+            analyze(pkg, source_to_analysis)
 
+def analyze(pkg, source_to_analysis):
+    """Run each analyzer on a source_to_analysis.
 
-
-def analyze(package):
-    """Run each analyzer on a package.
-
-    The package dict is in the queue. The keys are:
+    The source_to_analysis dict is in the queue. The keys are:
         plugin: the plugin module itself
         name: the package name
         version: the package version
         path: plugin default path for packages
         return: list with firehose reports
     """
-    plugin = package['plugin'].Plugin()
-    kiskadee.logger.debug(
-            'ANALYSIS: Downloading {} '
-            'source...'.format(package['name'])
-    )
-    compressed_source = plugin.get_sources(package)
-    pkg = (session.query(kiskadee.model.Package).
-            filter(kiskadee.model.Package.name == package['name']).
-            first())
-    if compressed_source:
-        kiskadee.logger.debug('ANALYSIS: Downloaded!')
-        kiskadee.logger.debug('ANALYSIS: Unpacking...')
-        reports = []
-        path = tempfile.mkdtemp()
-        shutil.unpack_archive(compressed_source, path)
-        with kiskadee.util.chdir(path):
-            kiskadee.logger.debug('ANALYSIS: Unpacked!')
-            analyzers = plugin.analyzers()
-            for analyzer in analyzers:
-                kiskadee.logger.debug('ANALYSIS: running %s ...' % analyzer)
-                try:
-                    analysis = kiskadee.analyzers.run(analyzer, path)
-                    firehose_report = kiskadee.converter.to_firehose(analysis,
-                                                                 analyzer)
-                    analysed_version = pkg.versions[-1].id
-                    kiskadee.logger.debug(
-                        "Saving analysis of {} on package {}-{}"
-                        .format(analyzer, pkg.name, pkg.versions[-1].number)
-                    )
-                    _save_source_analysis(
-                            analysed_version, firehose_report, analyzer
-                    )
-                    session.commit()
-                    kiskadee.logger.debug(
-                            'ANALYSIS: DONE {} analysis'.format(analyzer)
-                    )
-                except Exception as err:
-                    kiskadee.logger.debug('RUNNER: could not generate analysis')
-                    kiskadee.logger.debug(err)
-            # TODO: remove compressed/uncompressed files after the analysis
-    else:
-        kiskadee.logger.debug('RUNNER: invalid source dict')
+    plugin = source_to_analysis['plugin'].Plugin()
+    source_path = _path_to_uncompressed_source(source_to_analysis, plugin)
 
-def _save_source_analysis(version_id, analysis, analyzer):
+    if source_path is None:
+        return None
+
+    with kiskadee.helpers.chdir(source_path):
+        analyzers = plugin.analyzers()
+        for analyzer in analyzers:
+            kiskadee.logger.debug('ANALYSIS: running {} ...'.format(analyzer))
+            try:
+                analysis = kiskadee.analyzers.run(analyzer, source_path)
+                firehose_report = kiskadee.converter.to_firehose(analysis,
+                                                                 analyzer)
+                kiskadee.logger.debug(
+                    "Saving analysis of {} on package {}-{}"
+                    .format(analyzer, pkg.name, pkg.versions[-1].number)
+                )
+                _save_source_analysis(pkg, firehose_report, analyzer)
+                session.commit()
+                kiskadee.logger.debug(
+                        'ANALYSIS: DONE {} analysis'.format(analyzer)
+                )
+            except Exception as err:
+                kiskadee.logger.debug('RUNNER: could not generate analysis')
+                kiskadee.logger.debug(err)
+        # TODO: remove compressed/uncompressed files after the analysis
+
+def _save_source_analysis(package, analysis, analyzer):
+    version_id = package.versions[-1].id
     _analysis = kiskadee.model.Analysis()
     try:
         _analyzer = session.query(kiskadee.model.Analyzer).\
@@ -99,3 +90,21 @@ def _save_source_analysis(version_id, analysis, analyzer):
             "The required analyzer was not registered in kiskadee"
         )
         kiskadee.logger.debug(err)
+
+
+def _path_to_uncompressed_source(package, plugin):
+    kiskadee.logger.debug(
+            'ANALYSIS: Downloading {} '
+            'source...'.format(package['name'])
+    )
+    compressed_source = plugin.get_sources(package)
+    if compressed_source:
+        kiskadee.logger.debug('ANALYSIS: Downloaded!')
+        kiskadee.logger.debug('ANALYSIS: Unpacking...')
+        path = tempfile.mkdtemp()
+        shutil.unpack_archive(compressed_source, path)
+        kiskadee.logger.debug('ANALYSIS: Unpacked!')
+        return path
+    else:
+        kiskadee.logger.debug('RUNNER: invalid compressed source')
+        return None
