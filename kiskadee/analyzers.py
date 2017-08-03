2588b9e0905f40c42489bbf8ec11c3877e92f75a
David Carlos <ddavidcarlos1392@gmail.com>
2017-08-03 13:51:10 -0300

    Use check_output instead of the docker library.
        - related #38

--- kiskadee/analyzers.py
+++ kiskadee/analyzers.py
@@ -1,7 +1,6 @@
 """This module provides functions to run static analyzers."""
 
-import docker
-from subprocess import check_output
+import subprocess
 
 def run(analyzer, sources):
     """Run a static analyzer on a given package.
@@ -10,9 +9,8 @@ def run(analyzer, sources):
     `sources` is the absolute path for the uncompressed package. Returns
     a analysis results.
     """
-    volume = {sources: {'bind': '/src', 'mode': 'Z'}}
-    client = docker.from_env(version='auto')
-    uid = int(check_output("echo $UID", shell=True))
-    return client.containers.run(analyzer, '/src', volumes=volume,
-                                 environment={'KISKADEE_UID': uid},
-                                 stderr=True, stdout=True, tty=True)
+    volume = ''.join([sources, ':', '/src'])
+    uid = int(subprocess.check_output("echo $UID", shell=True))
+    return subprocess.check_output("docker run -e KISKADEE_UID={} "\
+                                "-v {} {}".format(uid, volume, analyzer),
+                                shell=True, stderr=subprocess.STDOUT)
--- util/dockerfiles/flawfinder/docker-entrypoint.sh
+++ util/dockerfiles/flawfinder/docker-entrypoint.sh
@@ -4,4 +4,4 @@ useradd -ms /bin/bash kiskadee -u $KISKADEE_UID
 chown -R kiskadee:kiskadee /src
 chmod -R 755 /src
 cd /src
-flawfinder -S .
+flawfinder -S --quiet .
