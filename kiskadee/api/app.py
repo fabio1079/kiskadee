2ff0ef225d545e679a1c4602a33540f72f03e2a2
David Carlos <ddavidcarlos1392@gmail.com>
2017-08-11 20:23:43 -0300

    Fix tests

--- kiskadee/api/app.py
+++ kiskadee/api/app.py
@@ -1,7 +1,7 @@
-import json
+"""Kiskadee API."""
 from flask import Flask, jsonify
 from flask import request
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 from kiskadee.database import Database
 from kiskadee.model import Package, Fetcher, Version, Analysis
@@ -12,16 +12,20 @@ kiskadee = Flask(__name__)
 db_session = Database().session
 CORS(kiskadee)
 
+
 @kiskadee.route('/fetchers')
 def index():
+    """Get the list of available fetchers."""
     if request.method == 'GET':
         fetchers = db_session.query(Fetcher).all()
         fetcher_schema = FetcherSchema(many=True)
         result = fetcher_schema.dump(fetchers)
         return jsonify({'fetcher': result.data})
 
+
 @kiskadee.route('/packages')
 def packages():
+    """Get the list of analyzed packages."""
     if request.method == 'GET':
         packages = db_session.query(Package).all()
         package_schema = PackageSchema(many=True)
@@ -31,17 +35,23 @@ def packages():
 
 @kiskadee.route('/analysis/<pkg_name>/<version>/')
 def package_analysis(pkg_name, version):
+    """Get the a analysis of some package version."""
     if request.method == 'GET':
         package = db_session.query(Package)\
                 .filter(Package.name == pkg_name).first().id
-        version = db_session.query(Version)\
+        version = (
+                db_session.query(Version)
                 .filter(Version.package_id == package).first().id
-        analysis = db_session.query(Analysis)\
+            )
+        analysis = (
+                db_session.query(Analysis)
                 .filter(Analysis.version_id == version).first()
+            )
 
         analysis_schema = AnalysisSchema()
         result = analysis_schema.dump(analysis)
         return jsonify({'analysis': result.data})
+
 
 if __name__ == '__main__':
     kiskadee.run('0.0.0.0')
--- kiskadee/api/serializers.py
+++ kiskadee/api/serializers.py
@@ -1,31 +1,46 @@
-from marshmallow import Schema, fields, ValidationError, pre_load
+"""Provide objects to serialize the kiskadee models."""
+
+from marshmallow import Schema, fields
+from kiskadee.model import Package, Fetcher, Analysis
+
 
 class FetcherSchema(Schema):
+    """Provide a serializer to the Fetcher model."""
+
     id = fields.Int()
     name = fields.Str()
     target = fields.Str()
     description = fields.Str()
 
     def make_object(self, data):
+        """Serialize a Fetcher object."""
         print('MAKING OBJECT FROM', data)
         return Fetcher(**data)
 
+
 class PackageSchema(Schema):
+    """Provide a serializer to the Package model."""
+
     id = fields.Int()
     name = fields.Str()
     target = fields.Str()
     fetcher_id = fields.Nested(FetcherSchema)
 
     def make_object(self, data):
+        """Serialize a Package object."""
         print('MAKING OBJECT FROM', data)
         return Package(**data)
 
+
 class AnalysisSchema(Schema):
+    """Provide a serializer to the Analysis model."""
+
     id = fields.Int()
     version_id = fields.Int()
     analyzer_id = fields.Int()
     raw = fields.Str()
 
     def make_object(self, data):
+        """Serialize a Analysis object."""
         print('MAKING OBJECT FROM', data)
         return Analysis(**data)
