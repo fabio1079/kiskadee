7cb5eb309a7d31c8728fa3d4755ca58734e79fd5
Athos Ribeiro <athoscr@fedoraproject.org>
2017-08-24 16:01:25 -0300

    Refactor script to build docker containers

--- docker_build.sh
+++ docker_build.sh
@@ -2,15 +2,15 @@
 #check_docker_client=`docker ps`
 docker ps 2> /dev/null
 if [ $? -ne 1 ]; then
-    echo "docker daemon properly configured"
+    echo "docker daemon properly configured. Building images..."
 else
     echo "docker daemon was not properly configured, is the service running?"
     exit
 fi
 
-(cd util/dockerfiles/cppcheck/ && docker build . -t cppcheck)
-(cd util/dockerfiles/flawfinder/ && docker build . -t flawfinder)
-(cd util/dockerfiles/clang-analyzer/ && docker build . -t clang-analyzer)
-(cd util/dockerfiles/frama-c/ && docker build . -t frama-c)
-
-
+pushd util/dockerfiles;
+for analyzer in `ls`; do
+  pushd $analyzer;
+  docker build . -t $analyzer;
+  popd
+done
